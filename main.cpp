eb08cb8455654f6e5196b1ed60eb442df005ec3a
Brock Burnett <brockaburnett95@gmail.com>
2020-05-12 23:04:25 -0500

    Changed: The main menu. Added: getChoice function

--- main.cpp
+++ main.cpp
@@ -3,12 +3,23 @@
 #include <fstream>
 using namespace std;
 
-void mainMenu(string name) {
-    cout << "Hello " << name << "! Lets get started on your Pokemon journey. Play the game by selecting the numbers of the options below.\n";
+int numOptions = 4;
+
+void getChoice(int& choice) {
+    cin >> choice;
+    while ((choice < 1) || (choice > numOptions)) {
+        cout << "Choose a valid option.\n";
+        cin >> choice;
+    }
+    return;
+}
+
+void mainMenu() {
+    cout << "What would you like to do?\n";
     cout << "\t1.) Go catch some Pokemon.\n";
     cout << "\t2.) Check your Pokedex.\n";
     cout << "\t3.) Save your game.\n";
-    cout << "\t4.) Quit game.\n";
+    cout << "\t4.) Quit the game.\n";
     return;
 }
 
@@ -17,7 +28,26 @@ int main() {
     cout << "Enter your name: ";
     string name;
     cin >> name;
-    mainMenu(name);
+    cout << "Hello " << name << "! Lets get started on your Pokemon journey.\n";
+
+    int choice = 0;
+
+    while (choice != numOptions) {
+        mainMenu();
+        getChoice(choice);
+        switch (choice) {
+            case 1:
+                cout << "Now we're out to catch some pokemon!\n";
+                break;
+            case 2:
+                cout << "Let's check out your pokedex progress.\n";
+                break;
+            case 3:
+                cout << "You've made progress, so lets save your game!\n";
+            case 4:
+                cout << "Quitting the game, thanks for playing!\n";
+        }
+    }
 
     return 0;
 }
